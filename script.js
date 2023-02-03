de8bd48c3cf18dc22b954f598a5b58d3a70e5eaa
Jack Massey <jackmassey@BCCSs-MacBook-Pro.local>
2023-02-03 10:14:16 -0600

    Script.js created, game variables created

---
+++ script.js
@@ -0,0 +1,12 @@
+//Text Adventure Game//
+let ans = prompt("Question 1")
+if (ans == 1) {
+    ans = prompt("Question 2")
+    if (ans == 1) {
+        ans = prompt("Question 3")
+    } else {
+        alert("Game Over Message")
+    }
+} else {
+    alert("Game Over Message")
+}
